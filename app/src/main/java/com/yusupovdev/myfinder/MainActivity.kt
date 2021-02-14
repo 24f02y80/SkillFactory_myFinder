536cf39731714c69e53a922af2285516549b15f1
Radion <yusupov.radion@gmail.com>
2021-02-14 17:07:49 +0300

    добавил передачу bundle

--- app/src/main/java/com/yusupovdev/myfinder/MainActivity.kt
+++ app/src/main/java/com/yusupovdev/myfinder/MainActivity.kt
@@ -8,28 +8,24 @@ import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-private lateinit var filmsAdapter: FilmListRecyclerAdapter
 class MainActivity : AppCompatActivity() {
-
     val filmDataBase = listOf(
             Film("American Ultra", R.drawable.americanultra, "This should be a description"),
             Film("Divergent", R.drawable.divergent, "This should be a description"),
             Film("Avatar", R.drawable.ktoia, "This should be a description"),
             Film("Кто Я", R.drawable.scale_1200, "This should be a description"),
-            Film("Марсиянин", R.drawable.marsianin, "This should be a description"),
+            Film("Пираты Корибского моря", R.drawable.marsianin, "This should be a description"),
             Film("Шпионский мост", R.drawable.tomhanks, "This should be a description"),
             Film("Titanik", R.drawable.titanik, "This should be a description"),
             Film("Высотка", R.drawable.visotka, "This should be a description")
     )
 
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        //       initMenuButtons()
-        //        button_menu.setOnClickListener{}
 
         main_recycler.apply {
             // Иницилизируем наш адаптер в конструктор передаем ананимно иницилизированный интнрфейс
@@ -43,9 +39,13 @@ class MainActivity : AppCompatActivity() {
                     bundle.putParcelable("film", film)
                     // запускаем наше активити
                     val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+                    intent.putExtras(bundle)
                     startActivity(intent)
                 }
-                override fun click(film: Film, position:Int) {}
+
+                override fun click(film: Film, position: Int)
+                {}
+
             })
             // Присваиваем адаптер
             adapter = filmsAdapter
@@ -87,26 +87,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
-        /*fun onClickToast(view: View){
-        Toast.makeText(this,"Меню", Toast.LENGTH_SHORT).show()
-*/
-
-/*private fun initMenuButtons() {
-    button_menu.setOnClickListener {
-        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-    }
-    btn_fav.setOnClickListener {
-        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-    }
-    btn_watch_later.setOnClickListener {
-        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-    }
-    btn_collection.setOnClickListener {
-        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-    }
-    btn_setting.setOnClickListener {
-        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-    }*/
     }
 }
