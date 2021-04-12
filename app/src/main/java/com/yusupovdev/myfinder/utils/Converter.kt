328c8f4637f7a566986549b201e7046aa523e217
Radion <yusupov.radion@gmail.com>
2021-04-13 00:52:52 +0300

    Теперь получается, что у нас с API приходит один объект, а в RV мы кладем список с другими объектами. создали конвертер

---
+++ app/src/main/java/com/yusupovdev/myfinder/utils/Converter.kt
@@ -0,0 +1,20 @@
+package com.yusupovdev.myfinder.utils
+
+import com.yusupovdev.myfinder.data.TmdbFilm
+import com.yusupovdev.myfinder.domain.Film
+
+object Converter {
+    fun convertApiListToDtoList(list: List<TmdbFilm>?): List<Film> {
+        val result = mutableListOf<Film>()
+        list?.forEach {
+            result.add(Film(
+                title = it.title,
+                poster = it.posterPath,
+                description = it.overview,
+                rating = it.voteAverage,
+                isInFavorites = false
+            ))
+        }
+        return result
+    }
+}
