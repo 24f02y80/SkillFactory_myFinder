3775d3844a47e3a26d648a8cccd12f37ec7a484f
Radion <yusupov.radion@gmail.com>
2021-04-13 03:40:54 +0300

    применим внедрение в нашу ViewModel

--- app/src/main/java/com/yusupovdev/myfinder/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/yusupovdev/myfinder/viewmodel/HomeFragmentViewModel.kt
@@ -2,15 +2,16 @@ package com.yusupovdev.myfinder.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.yusupovdev.myfinder.App
 import com.yusupovdev.myfinder.domain.Film
 import com.yusupovdev.myfinder.domain.Interactor
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
 
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
+    private val interactor: Interactor by inject()
 
     init {
         interactor.getFilmsFromApi(1, object : ApiCallback {
