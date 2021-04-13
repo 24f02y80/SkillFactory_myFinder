ccaf22cf9a1cf23c26cd860571ef9f7798787bbb
Radion <yusupov.radion@gmail.com>
2021-04-13 04:27:38 +0300

    сделали инжект в наш консюмер

--- app/src/main/java/com/yusupovdev/myfinder/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/yusupovdev/myfinder/viewmodel/HomeFragmentViewModel.kt
@@ -1,19 +1,20 @@
 package com.yusupovdev.myfinder.viewmodel
-
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yusupovdev.myfinder.App
 import com.yusupovdev.myfinder.domain.Film
 import com.yusupovdev.myfinder.domain.Interactor
-import org.koin.core.KoinComponent
-import org.koin.core.inject
+import javax.inject.Inject
 
-class HomeFragmentViewModel : ViewModel(), KoinComponent {
+class HomeFragmentViewModel : ViewModel() {
     val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
 
     //Инициализируем интерактор
-    private val interactor: Interactor by inject()
+    @Inject
+    lateinit var interactor: Interactor
 
     init {
+        App.instance.dagger.inject(this)
         interactor.getFilmsFromApi(1, object : ApiCallback {
             override fun onSuccess(films: List<Film>) {
                 filmsListLiveData.postValue(films)
