14e92393e4a12bf591769a848a69a69e9496e1da
yusup <radio_80@mail.ru>
2021-04-19 00:14:09 +0300

    Передаем конте кст в создание нашего компонента

--- app/src/main/java/com/yusupovdev/myfinder/App.kt
+++ app/src/main/java/com/yusupovdev/myfinder/App.kt
@@ -3,6 +3,9 @@ package com.yusupovdev.myfinder
 import android.app.Application
 import com.yusupovdev.myfinder.di.AppComponent
 import com.yusupovdev.myfinder.di.DaggerAppComponent
+import com.yusupovdev.myfinder.di.modules.DatabaseModule
+import com.yusupovdev.myfinder.di.modules.DomainModule
+import com.yusupovdev.myfinder.di.modules.RemoteModule
 
 class App : Application() {
     lateinit var dagger: AppComponent
@@ -11,7 +14,11 @@ class App : Application() {
         super.onCreate()
         instance = this
         //Создаем компонент
-        dagger = DaggerAppComponent.create()
+        dagger = DaggerAppComponent.builder()
+                .remoteModule(RemoteModule())
+                .databaseModule(DatabaseModule())
+                .domainModule(DomainModule(this))
+                .build()
     }
 
     companion object {
