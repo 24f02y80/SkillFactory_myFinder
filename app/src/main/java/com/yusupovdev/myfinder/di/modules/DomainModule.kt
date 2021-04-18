56b4f9ccc32ff1098df9bd12d83db84d3ee42173
yusup <radio_80@mail.ru>
2021-04-18 23:50:15 +0300

    связываем с DI

--- app/src/main/java/com/yusupovdev/myfinder/di/modules/DomainModule.kt
+++ app/src/main/java/com/yusupovdev/myfinder/di/modules/DomainModule.kt
@@ -1,15 +1,27 @@
 package com.yusupovdev.myfinder.di.modules
 
+import android.content.Context
 import com.yusupovdev.myfinder.data.MainRepository
 import com.yusupovdev.myfinder.data.TmdbApi
+import com.yusupovdev.myfinder.data.preference.PreferenceProvider
 import com.yusupovdev.myfinder.domain.Interactor
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class DomainModule {
+// Передаем контекст для SharedPreference через конструктор
+class DomainModule(val context: Context) {
+    // НАм нухно контекст провадить, поэтому создаем такой метод
+    @Provides
+    fun provideContext() = context
+
     @Singleton
     @Provides
-    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi) = Interactor(repo = repository, retrofitService = tmdbApi)
+    // Создаем кземпляр SharedPreferences
+    fun providePreferences(context: Context) = PreferenceProvider(context)
+
+    @Singleton
+    @Provides
+    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi, preferenceProvider: PreferenceProvider) = Interactor(repo = repository, retrofitService = tmdbApi, preferences = preferenceProvider)
 }
