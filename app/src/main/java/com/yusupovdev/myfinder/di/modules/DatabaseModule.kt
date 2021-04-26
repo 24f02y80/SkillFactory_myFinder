5d1fc447bd4ebae52c584b99ac9c576a153c317d
yusup <radio_80@mail.ru>
2021-04-27 00:42:03 +0300

    поменяли создание объекта DatabaseHelper на создание объекта FilmDao

--- app/src/main/java/com/yusupovdev/myfinder/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/yusupovdev/myfinder/di/modules/DatabaseModule.kt
@@ -1,7 +1,10 @@
 package com.yusupovdev.myfinder.di.modules
 
 import android.content.Context
+import androidx.room.Room
+import com.yusupovdev.myfinder.data.DAO.FilmDao
 import com.yusupovdev.myfinder.data.MainRepository
+import com.yusupovdev.myfinder.data.db.AppDatabase
 import com.yusupovdev.myfinder.data.db.DatabaseHelper
 import dagger.Module
 import dagger.Provides
@@ -11,9 +14,14 @@ import javax.inject.Singleton
 class DatabaseModule {
     @Singleton
     @Provides
-    fun provideDatabaseHelper(context: Context) = DatabaseHelper(context)
+    fun provideDatabaseHelper(context: Context) =
+        Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        "film_db"
+    ).build().filmDao()
 
     @Provides
     @Singleton
-    fun provideRepository(databaseHelper: DatabaseHelper) = MainRepository(databaseHelper)
+    fun provideRepository(filmDao: FilmDao) = MainRepository(filmDao)
 }
